2a38e023c9557962dd6557dbbbc20405f3b8270c
TwilightLemon <cz241126@live.com>
2018-08-11 15:19:33 +0800

    V1 Beta

---
+++ Sailfish/DownloadItems.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sailfish
+{
+    /// <summary>
+    /// DownloadItems.xaml 的交互逻辑
+    /// </summary>
+    public partial class DownloadItems : UserControl
+    {
+        public DownloadItems()
+        {
+            InitializeComponent();
+        }
+        public void SetIcon(String filename) {
+            icon.Source = ToImageSource(GetFileIcon(filename));
+        }
+        public string str = "";
+        /// <summary>
+        /// 设置暂停/继续状态
+        /// </summary>
+        /// <param name="i">0为暂停 1为继续</param>
+        public void SetDrow(int i) {
+            if (i == 0)
+                p.Data = Geometry.Parse("M910.8 303.6c-5.4-10.5-16.3-17.8-28.9-17.8-17.8 0-32.2 14.4-32.2 32.1 0 6 1.7 11.7 4.6 16.5l-0.1 0.1c26.9 52.4 42.1 111.8 42.1 174.7 0 211.6-171.6 383.2-383.2 383.2S129.8 720.8 129.8 509.2 301.4 126.1 513 126.1c62.5 0 121.5 15 173.6 41.5l0.2-0.4c4.6 2.6 10 4.1 15.7 4.1 17.8 0 32.2-14.4 32.2-32.1 0-13.1-7.9-24.4-19.3-29.4C654.6 78.9 585.9 61.5 513 61.5 265.7 61.5 65.3 262 65.3 509.2S265.7 956.9 513 956.9s447.7-200.4 447.7-447.7c0-74.1-18-144-49.9-205.6z M385.4 352.2V672c0 17.5 14.3 31.9 31.9 31.9 17.6 0 32-14.4 31.9-31.9V352.2c0-17.5-14.3-31.9-31.9-31.9-17.5 0-31.9 14.3-31.9 31.9zM578.9 352.2V672c0 17.5 14.3 31.9 31.9 31.9 17.5 0 31.9-14.4 31.9-31.9V352.2c0-17.5-14.3-31.9-31.9-31.9-17.5 0-31.9 14.3-31.9 31.9z M772.7 217.7a32.2 32.1 0 1 0 64.4 0 32.2 32.1 0 1 0-64.4 0Z");
+            else p.Data = Geometry.Parse("M512 0C229.228 0 0 229.228 0 512s229.228 512 512 512c282.773 0 512-229.228 512-512S794.772 0 512 0z m0 954.183C267.818 954.183 69.818 756.205 69.818 512c0-244.205 198-442.182 442.182-442.182 244.228 0 442.182 197.977 442.182 442.182S756.228 954.183 512 954.183z M750.545 481.772l-349.09-201.545a34.843 34.843 0 0 0-34.91 0 34.897 34.897 0 0 0-17.454 30.228v403.09c0 12.478 6.659 24 17.454 30.228A34.85 34.85 0 0 0 384 748.456a34.85 34.85 0 0 0 17.455-4.683l349.09-201.545A34.896 34.896 0 0 0 768 512c0-12.477-6.659-24-17.455-30.228zM418.909 653.079V370.92L663.272 512 418.909 653.079z");
+        }
+        public static Icon GetFileIcon(string fileName, bool largeIcon=true) { SHFILEINFO info = new SHFILEINFO(true); int cbFileInfo = Marshal.SizeOf(info); SHGFI flags; if (largeIcon) flags = SHGFI.Icon | SHGFI.LargeIcon | SHGFI.UseFileAttributes; else flags = SHGFI.Icon | SHGFI.SmallIcon | SHGFI.UseFileAttributes; SHGetFileInfo(fileName, 256, out info, (uint)cbFileInfo, flags); return Icon.FromHandle(info.hIcon); }
+        [DllImport("Shell32.dll")] private static extern int SHGetFileInfo(string pszPath, uint dwFileAttributes, out SHFILEINFO psfi, uint cbfileInfo, SHGFI uFlags);[StructLayout(LayoutKind.Sequential)] private struct SHFILEINFO { public SHFILEINFO(bool b) { hIcon = IntPtr.Zero; iIcon = 0; dwAttributes = 0; szDisplayName = ""; szTypeName = ""; } public IntPtr hIcon; public int iIcon; public uint dwAttributes;[MarshalAs(UnmanagedType.LPStr, SizeConst = 260)] public string szDisplayName;[MarshalAs(UnmanagedType.LPStr, SizeConst = 80)] public string szTypeName; }; private enum SHGFI { SmallIcon = 0x00000001, LargeIcon = 0x00000000, Icon = 0x00000100, DisplayName = 0x00000200, Typename = 0x00000400, SysIconIndex = 0x00004000, UseFileAttributes = 0x00000010 }
+        public static ImageSource ToImageSource(Icon icon)
+        {
+            ImageSource imageSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(
+                icon.Handle,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+            return imageSource;
+        }
+
+        private void UserControl_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Size.Text == "已完成")
+                Process.Start(str);
+        }
+    }
+}
---
+++ Sailfish/MainWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+
+namespace Sailfish
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<XL.DownTaskParam> DownloadDatas = new List<XL.DownTaskParam>();
+        List<DownloadItems> DownloadControls = new List<DownloadItems>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            T.Interval = 500;
+            T.Tick += delegate {
+                XL.XL_QueryTaskInfoEx(a, info);
+                if (info.stat!=XL.DOWN_TASK_STATUS.TSC_COMPLETE)
+                {
+                    DownloadControls[0].pro.Value = (int)(info.fPercent * 100);
+                    title.Text = "Sailfish   --" + HumanReadableFilesize(info.nSpeed)+"/s";
+                }
+                else
+                {
+                    T.Stop();
+                    DownloadControls[0].pro.Value = DownloadControls[0].pro.Minimum;
+                    a = new IntPtr(0);
+                    DownloadControls[0].Size.Text = "已完成";
+                    title.Text = "Sailfish";
+                    DownloadDatas.RemoveAt(0);
+                    DownloadControls.RemoveAt(0);
+                    if (DownloadControls.Count >0) {
+                        a = XL.XL_CreateTask(DownloadDatas[0]);
+                        XL.XL_StartTask(a);
+                        T.Start();
+                        DownloadControls[0].bors.MouseDown += Bors_MouseDown;
+                    }
+                }
+            };
+        }
+        public static long GetRemoteHTTPFileSize(string sURL)
+        {
+            long size = 0L;
+            try
+            {
+                System.Net.HttpWebRequest request = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(sURL);
+                request.Method = "HEAD";
+                System.Net.HttpWebResponse response = (System.Net.HttpWebResponse)request.GetResponse();
+                size = response.ContentLength;
+                response.Close();
+            }
+            catch
+            {
+                size = 0L;
+            }
+            return size;
+        }
+        private String HumanReadableFilesize(double size)
+        {
+            String[] units = new String[] { "B", "KB", "MB", "GB", "TB", "PB" };
+            double mod = 1024.0;
+            int i = 0;
+            while (size >= mod)
+            {
+                size /= mod;
+                i++;
+            }
+            return Math.Round(size) + units[i];
+        }
+        System.Windows.Forms.Timer T = new System.Windows.Forms.Timer();
+        private void url_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                DownloadPath.Text = "D://"+System.IO.Path.GetFileName(DownloadUrl.Text);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            XL.XL_Init();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            XL.XL_UnInit();
+        }
+        XL.DownTaskInfo info = new XL.DownTaskInfo();
+        private IntPtr a = new IntPtr(0);
+        private void CLOSE_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var b = new DoubleAnimation(1, 0, TimeSpan.FromSeconds(0.2));
+            b.Completed += delegate { Environment.Exit(0); };
+            BeginAnimation(OpacityProperty, b);
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            (Resources["OnMouseDown2"] as Storyboard).Begin();
+            grid.Visibility = Visibility.Collapsed;
+            FileInfo f = new FileInfo(DownloadPath.Text);
+            if (f.Exists)
+                f.Delete();
+            String FileName = f.Name;
+            String SavePath = f.DirectoryName;
+            String url = DownloadUrl.Text;
+            DownloadItems dl = new DownloadItems();
+            dl.Size.Text =HumanReadableFilesize(GetRemoteHTTPFileSize(url));
+            dl.name.Text = FileName;
+            dl.SetIcon(FileName);
+            dl.Width = sv.ActualWidth;
+            dl.str = DownloadPath.Text;
+            wpList.Children.Add(dl);
+            DownloadControls.Add(dl);
+            DownloadUrl.Text = "";
+            DownloadPath.Text = "";
+            XL.DownTaskParam p = new XL.DownTaskParam()
+            {
+                szTaskUrl = url,
+                szFilename =FileName,
+                szSavePath = SavePath
+            };
+            DownloadDatas.Add(p);
+            if (DownloadDatas.Count == 1) {
+                a = XL.XL_CreateTask(p);
+                XL.XL_StartTask(a);
+                T.Start();
+                dl.bors.MouseDown += Bors_MouseDown;
+            }
+        }
+        int i = 0;
+        private void Bors_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (i == 0){
+                i = 1;
+                XL.XL_StopTask(a);
+            }
+            else {
+                i = 0;
+                XL.XL_StartTask(a);
+                T.Start();
+            }
+            DownloadControls[0].SetDrow(i);
+        }
+    }
+}
